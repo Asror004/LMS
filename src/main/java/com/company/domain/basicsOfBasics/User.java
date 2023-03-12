02dab0670ce9b789a987ddee69ba201451f8a661
asror <asroryuldashev57@gmail.com>
2023-03-12 14:36:12 +0500

    fixed

--- src/main/java/com/company/domain/basicsOfBasics/User.java
+++ src/main/java/com/company/domain/basicsOfBasics/User.java
@@ -5,6 +5,7 @@ import com.company.domain.basic.Group;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
@@ -32,7 +33,7 @@ public class User {
     @ManyToOne(cascade = CascadeType.MERGE)
     private Address address;
     @Column(nullable = false)
-    private LocalDateTime birthDate;
+    private LocalDate birthDate;
     @Column(nullable = false)
     private String passport;
     @Column(nullable = false)
