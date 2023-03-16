3a3e8beff725d37bd209305a7093b3cde1a77c9c
ElbekKholmatov <exolmatov99@gmail.com>
2023-03-16 14:44:00 +0500

    group 9%

--- src/main/java/com/company/service/GroupService.java
+++ src/main/java/com/company/service/GroupService.java
@@ -1,15 +1,24 @@
-package com.company.service;
-import com.company.repository.LanguageRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class GroupService {
-
-    private final LanguageRepository languageRepository;
-    private final PasswordEncoder passwordEncoder;
-
-
-}
+//package com.company.service;
+//import com.company.domain.basic.Group;
+//import com.company.repository.LanguageRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class GroupService {
+//
+//    private final LanguageRepository languageRepository;
+//    private final PasswordEncoder passwordEncoder;
+//
+//
+//    public List<Group> findAll() {
+//
+//    }
+//
+//    public void create() {
+//    }
+//}
