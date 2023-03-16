0cc2098c7f1e310f088f997c45e0b7e354c2f634
mirsaidkarimullaev <mirsaidkarimullaev@gmail.com>
2023-03-16 15:45:29 +0500

    student fixed

--- src/main/java/com/company/controller/StudentController.java
+++ src/main/java/com/company/controller/StudentController.java
@@ -1,90 +1,112 @@
 package com.company.controller;
 
+import com.company.domain.basic.Subject;
+import com.company.repository.SubjectRepository;
+import com.company.security.UserSession;
+import org.springframework.context.ApplicationContext;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
+    private final UserSession session;
+    private final SubjectRepository subjectRepository;
+
+    public StudentController(UserSession session, SubjectRepository subjectRepository) {
+        this.session = session;
+        this.subjectRepository = subjectRepository;
+    }
+
     @GetMapping("/main")
     @PreAuthorize("hasRole('STUDENT')")
-    public String main(){
+    public String main() {
         return "studentPages/main";
     }
 
     @GetMapping("/news")
     @PreAuthorize("hasRole('STUDENT')")
-    public String news(){
+    public String news() {
         return "studentPages/news";
     }
 
     @GetMapping("/my-courses")
     @PreAuthorize("hasRole('STUDENT')")
-    public String myCourses(){
+    public String myCourses(Model model) {
+//        Object attribute = model.getAttribute("usersession.getuserId");
+//        System.out.println("attribute = " + attribute);
 
+        Integer id = session.getId();
+        System.out.println("id = " + id);
+
+        List<Subject> myCourses = subjectRepository.findAllSubjectsOfStudentByUserId(id);//forEach(System.out::println);
+        model.addAttribute("myCourses", myCourses);
         return "studentPages/my-courses";
     }
 
     @GetMapping("/schedule")
     @PreAuthorize("hasRole('STUDENT')")
-    public String schedule(){
+    public String schedule() {
         return "studentPages/schedule";
     }
 
     @GetMapping("/retake")
     @PreAuthorize("hasRole('STUDENT')")
-    public String retake(){
+    public String retake() {
         return "studentPages/retake";
     }
 
     @GetMapping("/finals")
     @PreAuthorize("hasRole('STUDENT')")
-    public String finals(){
+    public String finals() {
         return "studentPages/finals";
     }
 
     @GetMapping("/study-plan")
     @PreAuthorize("hasRole('STUDENT')")
-    public String studyPlan(){
+    public String studyPlan() {
         return "studentPages/study-plan";
     }
 
     @GetMapping("/info")
     @PreAuthorize("hasRole('STUDENT')")
-    public String info(){
+    public String info() {
         return "studentPages/info";
     }
 
     @GetMapping("/survey")
     @PreAuthorize("hasRole('STUDENT')")
-    public String survey(){
+    public String survey() {
         return "studentPages/survey";
     }
 
     @GetMapping("/certificate")
     @PreAuthorize("hasRole('STUDENT')")
-    public String certificate(){
+    public String certificate() {
         return "studentPages/certificate";
     }
 
     @GetMapping("/olympiad")
     @PreAuthorize("hasRole('STUDENT')")
-    public String olympiad(){
+    public String olympiad() {
         return "studentPages/olympiad";
     }
 
     @GetMapping("/diploma-work")
     @PreAuthorize("hasRole('STUDENT')")
-    public String diplomaWork(){
+    public String diplomaWork() {
         return "studentPages/diploma-work";
     }
 
     @GetMapping("/subject")
     @PreAuthorize("hasRole('STUDENT')")
-    public String subject(){
+    public String subject() {
         return "studentPages/subject";
     }
 
--- src/main/java/com/company/repository/SubjectRepository.java
+++ src/main/java/com/company/repository/SubjectRepository.java
@@ -1,11 +1,16 @@
 package com.company.repository;
 
-import com.company.domain.auth.AuthUser;
 import com.company.domain.basic.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
+import java.util.List;
 
 
 public interface SubjectRepository extends JpaRepository<Subject, Integer> {
+    @Query("select s.subjects from User s where s.authUserId = ?1")
+    List<Subject> findAllSubjectsOfStudentByUserId(Integer userId);
+
+    // Teacher lani Ismini qaytarish kere subject lar bo'yicha
+
 }
