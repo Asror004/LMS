1d21211c46821775a0fcecaf06b7d2cb3e44d83b
ElbekKholmatov <exolmatov99@gmail.com>
2023-03-20 10:57:01 +0500

    group 29%

--- src/main/java/com/company/controller/GroupController.java
+++ src/main/java/com/company/controller/GroupController.java
@@ -1,95 +1,85 @@
-//package com.company.controller;
-//
-//import com.company.domain.basic.Group;
-//import com.company.service.GroupService;
-//import jakarta.validation.Valid;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.access.prepost.PreAuthorize;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.validation.BindingResult;
-//import org.springframework.web.bind.annotation.*;
-//
-//@Controller
-//@RequiredArgsConstructor
-//@RequestMapping("/admin/group")
-//public class GroupController {
-//    private final GroupService groupService;
-//
-//    @GetMapping
-//    @PreAuthorize("hasRole('ADMIN')")
-//    public String main(Model model) {
-//        model.addAttribute("group", new Group());
-//        model.addAttribute("groups", groupService.findAll());
-//        return "crud/group/main";
-//    }
-//
-//
-//    @GetMapping("/create")
-//    @PreAuthorize("hasAnyAuthority(T(com.company.permissions.AdminPermissions).HAS_CREATE_GROUP_PERMISSION)")
-//    public String create(Model model) {
-//        model.addAttribute("group", new Group());
-//        return "crud/group/create";
-//    }
-//
-//    @PostMapping("/create")
-//    @PreAuthorize("hasAnyAuthority(T(com.company.permissions.AdminPermissions).HAS_CREATE_GROUP_PERMISSION)")
-//    public String createPost(@RequestParam String name, @RequestParam Integer count,Model model) {
-//        if (name.isBlank()) {
-//            model.addAttribute("field","field.blank");
-//            return "crud/group/create";
-//        }
-//        if (name.length()>10) {
-//            model.addAttribute("field","Must be max 10");
-//            return "crud/group/create";
-//        }
-//        if (count<1) {
-//            model.addAttribute("count","must.positive");
-//            return "crud/group/create";
-//        }
-////        TODO: CHECK IS ALREADY EXISTS
-////        Group byName = groupService.findByName(name.toUpperCase() + "-" + count);
-////        System.out.println(byName);
-////        if (byName != null) {
-////            model.addAttribute("field","group.exists");
-////        }
-//
-//
-//        groupService.create();
-//        return "redirect:/admin/group";
-//    }
-//
-//
-//    @GetMapping("/update")
-//    @PreAuthorize("hasRole('ADMIN')")
-//    public String update() {
-//        return "crud/group/main";
-//    }
-//
-//    @PostMapping("/update")
-//    @PreAuthorize("hasRole('ADMIN')")
-//    public String updatePost(@Valid @ModelAttribute("group") DeleteGroupDTO groupDTO, BindingResult errors) {
-//        if (errors.hasErrors()) {
-//            return "crud/group/main";
-//        }
-//        groupService.update(groupDTO);
-//        return "redirect:/admin/group";
-//
-//    }
-//    @GetMapping("/delete")
-//    @PreAuthorize("hasRole('ADMIN')")
-//    public String delete() {
-//        return "redirect:/admin/group";
-//
-//    }
-//
-//    @PostMapping("/delete")
-//    @PreAuthorize("hasRole('ADMIN')")
-//    public String deletePost(@Valid @ModelAttribute("group") DeleteGroupDTO groupDTO, BindingResult errors) {
-//        if (errors.hasErrors()) {
-//            return "crud/group/main";
-//        }
-//        groupService.delete(groupDTO);
-//        return "redirect:/admin/group";
-//    }
-//}
+package com.company.controller;
+
+import com.company.domain.basic.Group;
+import com.company.dto.groupDTO.CreateGroupDTO;
+import com.company.dto.groupDTO.*;
+import com.company.service.FacultyService;
+import com.company.service.GroupService;
+import com.company.service.TeacherService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/admin/group")
+public class GroupController {
+    private final GroupService groupService;
+    private FacultyService facultyService;
+    private TeacherService teacherService;
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public String main(Model model) {
+        model.addAttribute("group", new Group());
+        model.addAttribute("groups", groupService.findAll());
+        return "crud/group/main";
+    }
+
+
+    @GetMapping("/create")
+    @PreAuthorize("hasAnyAuthority(T(com.company.permissions.AdminPermissions).HAS_CREATE_GROUP_PERMISSION)")
+    public String create(Model model) {
+        model.addAttribute("group", new Group());
+        model.addAttribute("faculties", facultyService.findAll());
+        model.addAttribute("teachers", teacherService.findAll());
+        return "crud/group/create";
+    }
+
+    @PostMapping("/create")
+    @PreAuthorize("hasAnyAuthority(T(com.company.permissions.AdminPermissions).HAS_CREATE_GROUP_PERMISSION)")
+    public String createPost(@Valid @ModelAttribute("group")CreateGroupDTO groupDTO, BindingResult errors) {
+        if (errors.hasErrors()) {
+            return "crud/group/create";
+        }
+        groupService.create(groupDTO);
+        return "redirect:/admin/group";
+    }
+
+
+    @GetMapping("/update")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String update() {
+        return "crud/group/main";
+    }
+
+    @PostMapping("/update")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String updatePost(@Valid @ModelAttribute("group") UpdateGroupDTO groupDTO, BindingResult errors) {
+        if (errors.hasErrors()) {
+            return "crud/group/main";
+        }
+        groupService.update(groupDTO);
+        return "redirect:/admin/group";
+
+    }
+    @GetMapping("/delete")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String delete() {
+        return "redirect:/admin/group";
+    }
+
+    @PostMapping("/delete")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String deletePost(@Valid @ModelAttribute("group") DeleteGroupDTO groupDTO, BindingResult errors) {
+        if (errors.hasErrors()) {
+            return "crud/group/main";
+        }
+        groupService.delete(groupDTO);
+        return "redirect:/admin/group";
+    }
+}
--- src/main/java/com/company/service/GroupService.java
+++ src/main/java/com/company/service/GroupService.java
@@ -1,24 +1,45 @@
-//package com.company.service;
-//import com.company.domain.basic.Group;
-//import com.company.repository.LanguageRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class GroupService {
-//
-//    private final LanguageRepository languageRepository;
-//    private final PasswordEncoder passwordEncoder;
-//
-//
-//    public List<Group> findAll() {
-//
-//    }
-//
-//    public void create() {
-//    }
-//}
+package com.company.service;
+import com.company.domain.basic.Group;
+import com.company.dto.groupDTO.CreateGroupDTO;
+import com.company.dto.groupDTO.DeleteGroupDTO;
+import com.company.dto.groupDTO.UpdateGroupDTO;
+import com.company.mappers.group.GroupMapper;
+import com.company.repository.GroupRepository;
+import com.company.repository.LanguageRepository;
+import com.company.repository.RoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GroupService {
+    private final GroupMapper groupMapper;
+
+    private final GroupRepository groupRepository;
+
+    public List<Group> findAll() {
+        return groupRepository.findAll();
+    }
+
+    public void delete(DeleteGroupDTO groupDTO) {
+        Group group = groupMapper.deleteGroup(groupDTO);
+        group.setUpdatedAt(LocalDateTime.now());
+        groupRepository.delete(group);
+    }
+
+    public void update(UpdateGroupDTO groupDTO) {
+//        TODO: add update logic
+        Group group = groupMapper.updateGroup(groupDTO);
+        group.setUpdatedAt(LocalDateTime.now());
+        groupRepository.save(group);
+    }
+
+    public void create(CreateGroupDTO groupDTO) {
+        Group group = groupMapper.fromCreateDTO(groupDTO);
+        groupRepository.save(group);
+    }
+}
