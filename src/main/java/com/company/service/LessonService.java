2821e7c5f4219fadbcf612044685c07d399f7458
asror <asroryuldashev57@gmail.com>
2023-03-21 14:39:13 +0500

    lesson create started

---
+++ src/main/java/com/company/service/LessonService.java
@@ -0,0 +1,19 @@
+package com.company.service;
+
+import com.company.domain.basic.Faculty;
+import com.company.repository.FacultyRepository;
+import com.company.repository.LessonRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class LessonService {
+    private final LessonRepository repository;
+    private final FacultyRepository facultyRepository;
+    public List<Faculty> getFaculties(){
+        return facultyRepository.findAll();
+    }
+}
