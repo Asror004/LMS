e002f3f1733ad15d9d80a8444310e31a6d66ba56
ElbekKholmatov <exolmatov99@gmail.com>
2023-03-22 15:43:20 +0500

    group 55%

--- src/main/java/com/company/dto/groupDTO/CreateGroupDTO.java
+++ src/main/java/com/company/dto/groupDTO/CreateGroupDTO.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.UniqueElements;
 import org.springframework.context.MessageSource;
@@ -17,18 +18,14 @@ import org.springframework.context.MessageSource;
 )
 public record CreateGroupDTO(
         @NotNull(message = "Course is required")
-//        @UniqueElements(message = "Group with this name already exists")
         Integer course,
 
-//        @NotNull(message = "Name is required")
         @NotBlank(message = "Name is required")
         String name,
 
-//        @NotNull(message = "Faculty is required")
-        @NotNull(message = "Faculty is required")
+        @Size(min = 1, max = 8, message = "Faculty is required")
         Integer facultyId,
 
-//        @NotNull(message = "Teacher is required")
         @NotNull(message = "Teacher is required")
         Integer teacherId
 
